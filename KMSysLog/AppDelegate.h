76cc91e50a27f9ae8f02a8f26ecc6bf37c52587a
kimilee123 <kimilee@kimileedeMacBook-Pro.local>
2017-03-02 16:49:23 +0800

    creat poject

---
+++ KMSysLog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KMSysLog
+//
+//  Created by kimilee on 2017/3/2.
+//  Copyright © 2017年 kimilee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KMSysLog/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  KMSysLog
+//
+//  Created by kimilee on 2017/3/2.
+//  Copyright © 2017年 kimilee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor orangeColor];
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
